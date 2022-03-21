2d24d9dc3a6fab42459fb702855dff17704da759
Glizy112 <77720813+Glizy112@users.noreply.github.com>
2022-03-22 01:09:04 +0530

    First Commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,28 @@
+//import React from 'react'
+import Head from 'next/head'
+import Link from "next/link"
+import Script from 'next/script'
+import Layout from '../../components/layout'
+
+const firstPost = () => {
+  return (
+      <Layout>
+      <Head>
+        <title> First Post </title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={()=>console.log("Loaded Successfully...", window.FB)}
+      />
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+        </h2>
+    </Layout>
+  )
+}
+
+export default firstPost
